2d3c696b2ca7eb839d38534ec9cdc0882dabd43b
Larry Lo <gaogaigargalden@gmail.com>
2024-03-04 07:44:17 +0800

    feat: first commit

---
+++ src/main/java/com/larry/google/geminispringboot/GeminiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.larry.google.geminispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larry/google/geminispringboot/GeminiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larry.google.geminispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
